83ef5a2cef7de18fe64cd4f156b23f4979cc3535
a <a@DESKTOP-UJAHHIE>
2025-07-08 21:20:57 +0530

    new change

--- Java_Project/src/java_Package/first.java
+++ Java_Project/src/java_Package/first.java
@@ -3,8 +3,9 @@ package java_Package;
 public class first {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
-		System.out.println("hello automation");
+		System.out.println("hello git1");
+		System.out.println("hello automation1");
+		System.out.println("hello java");
 		
 		
 
